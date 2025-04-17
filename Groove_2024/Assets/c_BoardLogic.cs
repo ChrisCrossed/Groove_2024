061b400d94fb7bf21dd7c2bbe6da9baee34b0437
ChrisCrossed <chriscrossed.86@gmail.com>
2025-04-17 15:32:06 -0600

    Continuing Progress on Board Visuals Array

--- Groove_2024/Assets/c_BoardLogic.cs
+++ Groove_2024/Assets/c_BoardLogic.cs
@@ -20,12 +20,26 @@ public class c_BoardLogic : MonoBehaviour
     List<GameObject> Squircles;
     void InitializeSquircles()
     {
+        // Just a temporary test. Remember that you want Backdrop squares instead
         Squircles = new List<GameObject>();
 
-        GameObject tempSquircle = GameObject.Instantiate(SquirclePrefab);
-        tempSquircle.gameObject.transform.localScale = new Vector3(0.85f, 0.85f, 1.0f);
-        tempSquircle.name = "TestSquircle";
-        tempSquircle.transform.position = new Vector3(4, 4, 4);
+        for(int y = 0; y < 3; y++)
+        {
+            for(int x = 0; x < 12; x++)
+            {
+                GameObject tempSquircle = GameObject.Instantiate(SquirclePrefab);
+                tempSquircle.gameObject.transform.localScale = new Vector3(0.85f, 0.85f, 1.0f);
+                tempSquircle.name = "TestSquircle";
+
+                // Bottom Left Corner Pos + (.85 scale + 1.25 buffer) * GridPos
+                // BottomLeft = Vector3(-7.5,-1.25,0)
+                Vector3 newPos = new Vector3(-7.5f, -1.25f, 0);
+                newPos.x += (1.25f) * x;
+                newPos.y += (1.25f) * y;
+                tempSquircle.transform.position = newPos;
+            }
+        }
+        
     }
 
     // Update is called once per frame
