942d6cf688064f6b1ffe8765d6398235798cb905
ChrisCrossed <chriscrossed.86@gmail.com>
2025-04-20 15:42:28 -0600

    Updating BoardObject materials & positioning

--- Groove_2024/Assets/c_BoardLogic.cs
+++ Groove_2024/Assets/c_BoardLogic.cs
@@ -6,6 +6,15 @@ public class c_BoardLogic : MonoBehaviour
 {
     GameObject GameLogicObject;
     GameLogic GameLogic;
+
+    [SerializeField] private Material Mat_AlphaBlock;
+    [SerializeField] private Material Mat_BravoBlock;
+    [SerializeField] private Material Mat_EmptyBlock;
+
+    [SerializeField] private Material Mat_AlphaBackdrop;
+    [SerializeField] private Material Mat_BravoBackdrop;
+    [SerializeField] private Material Mat_EmptyBackdrop;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,21 +44,53 @@ public class c_BoardLogic : MonoBehaviour
             for(int x = 0; x < BoardWidth; x++)
             {
                 GameObject tempBackdrop = GameObject.Instantiate(BackdropPrefab);
+                
                 // tempBackdrop.gameObject.transform.localScale = new Vector3(0.85f, 0.85f, 1.0f);
                 tempBackdrop.name = "Backdrop";
+                
 
-                // Bottom Left Corner Pos + (.85 scale + 1.25 buffer) * GridPos
-                // BottomLeft = Vector3(-7.5,-1.25,0)
+                
+
                 float leftPos = BoardWidth / 2f * -1f;
-                Vector3 newPos = new Vector3(leftPos, -1.25f, 0);
-                newPos.x += (1.25f) * x;
-                newPos.y += (1.25f) * y;
-                tempBackdrop.transform.position = newPos;
+                Vector3 backdropPos = new Vector3(leftPos, -1.25f, 0);
+                
+
+                backdropPos.x += (1.25f) * x;
+                backdropPos.y += (1.25f) * y;
+                tempBackdrop.transform.position = backdropPos;
+
+                
 
                 tempBackdrop.transform.SetParent( GameObject.Find("BackdropArray").transform );
                 BackdropObjects.Add( tempBackdrop );
             }
         }
+    }
+
+    public void AddBlockToBoard(Vector2 _pos, BoardObject _boardObjectType)
+    {
+        if(!(_boardObjectType == BoardObject.Alpha_Active || _boardObjectType == BoardObject.Bravo_Active))
+        {
+            print("WRONG BOARD OBJECT TYPE - 'AddBlockToBoard' (c_BoardLogic)");
+            return;
+        }
+
+        GameObject tempSquircle = GameObject.Instantiate(SquirclePrefab);
+        tempSquircle.name = "Alpha_Squircle";
+        if(_boardObjectType == BoardObject.Bravo_Active)
+        {
+            tempSquircle.name = "Bravo_Squircle";
+        }
+
+        tempSquircle.gameObject.transform.localScale = new Vector3(0.7f, 0.7f, 1.0f);
+
+        float leftPos = BoardWidth / 2f * -1f;
+        Vector3 squirclePos = new Vector3(leftPos, -1.25f, 0);
+
+        squirclePos.x = (1.25f) * _pos.x;
+        squirclePos.y = (1.25f) * _pos.y;
+        squirclePos.z += -0.35f;
+        tempSquircle.transform.position = squirclePos;
     }
 
     int BoardWidth;
@@ -72,6 +113,71 @@ public class c_BoardLogic : MonoBehaviour
         newCameraPos.y = (BoardHeight / 2f) - (BlockScale / 2f);
         newCameraPos.z = -12f;
         CameraObject.transform.position = newCameraPos;
+    }
+
+    float BlockTimeChange_MAX = 0.2f;
+    IEnumerator ChangeBlockColor(Vector2 _pos, BoardObject changeToBlock_)
+    { 
+        bool isDone = false;
+        float fTimer = 0f;
+
+        GameObject blockObj    = GetObjAtPosition(new Vector2(1, 1), true);
+        GameObject backdropObj = GetObjAtPosition(new Vector2(1, 1), false);
+
+        Material oldBlockMat = null;
+        Material oldBackdropMat = null;
+        Material newMat = null;
+
+        switch (changeToBlock_)    
+        {
+            case BoardObject.Alpha_Active:
+            case BoardObject.Alpha_Static:
+                newMat = Mat_AlphaBlock;
+                break;
+            case BoardObject.Bravo_Active:
+            case BoardObject.Bravo_Static:
+                newMat = Mat_BravoBlock;
+                break;
+            case BoardObject.Filled:
+                break;
+            case BoardObject.Filled_Alpha:
+                break;
+            case BoardObject.Filled_Bravo:
+                break;
+            case BoardObject.Ghost:
+                break;
+            case BoardObject.Empty:
+            default:
+                newMat = Mat_EmptyBlock;
+                break;
+        }
+
+        while(!isDone)
+        {
+            fTimer += Time.deltaTime;
+            if(fTimer > BlockTimeChange_MAX )
+            {
+                fTimer = 1f;
+                isDone = true;
+            }
+
+            float perc = fTimer / BlockTimeChange_MAX;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    /// <summary>
+    /// Returns the GameObject at the board position given
+    /// </summary>
+    /// <param name="_pos"></param> Position on the board
+    /// <param name="_isBlock"></param> True = Block Object, False = Backdrop Object
+    /// <returns></returns>
+    GameObject GetObjAtPosition(Vector2 _pos, bool _isBlock)
+    {
+        return null;
     }
 
     // Update is called once per frame
