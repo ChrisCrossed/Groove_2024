7dd5a46e57fca7c6bb78477cc0a69110a2b8141c
ChrisCrossed <chriscrossed.86@gmail.com>
2025-10-31 21:52:32 -0600

    Beginning Work on Tutorial

--- Groove_2024/Assets/GrooveThemes.cs
+++ Groove_2024/Assets/GrooveThemes.cs
@@ -20,6 +20,8 @@ public class GrooveThemes : MonoBehaviour
 
     [SerializeField] int boardWidth;
 
+    [SerializeField] bool IsTutorial = false;
+
     private void Start()
     {
         ReadDataFromFile();
